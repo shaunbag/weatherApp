013c620484b3472735908a5f6cb31c3dc33bdf48
Shaunbag <shaunbag@hotmail.com>
2021-01-03 12:02:35 +0000

    implementing state and props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,39 @@
 import './App.css';
 import React from 'react';
-import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      location: 'Norwich',
+      temp: 40,
+      clouds: 'Overcast',
+      precip: 30
+    }
+  }
   
+  weatherSearch() {
+    this.setState({
+      location: 'London',
+      temp: 35,
+      clouds: 'None',
+      precip: 50
+    });
+  }
 
-  render(){
+render(){
   return (
     <div className="App">
       <header className="App-header">
 
         <h1 className="title" >Welcome to Weather App</h1>
-
-        <SearchBar />
-        <SearchResults />
+        <div>
+            <input type="text" className="searchBar"   placeholder="Type your town or city here"></input>
+            <button onClick={() => this.weatherSearch()}>Search</button>
+        </div>
+        <SearchResults location={this.state.location} temp={this.state.temp} clouds={this.state.clouds} precip={this.state.precip}/>
+        
 
       </header>
     </div>
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import './SearchResults.css';
 
 
 
 export class SearchResults extends React.Component {
     render() {
         return (
+            
             <div className="results">
-                <p>{Date()}</p>
+                <p>{Date().slice(3, 21)}</p>
                 <p>The Weather In {this.props.location} is</p>
-                <p>Temp: 39{'\u00B0'}C</p>
-                <p>Cloud Cover: none</p>
-                <p>Precipitation: 30%</p>
+                <p>Temp: {this.props.temp}{'\u00B0'}C</p>
+                <p>Cloud Cover: {this.props.clouds}</p>
+                <p>Precipitation: {this.props.precip}%</p>
+                <img src="../../../favicon.ico" alt=""></img>
             </div>
-        )
-    }
+        )}
 }
+
